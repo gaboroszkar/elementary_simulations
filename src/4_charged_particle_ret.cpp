11e321434c3e63a2447620168824b4ed4ba30ab6
Gábor Oszkár Dénes <gaboroszkar@protonmail.com>
2026-06-09 10:46:32 +0200

    Add charged particle retarded solution but only for one charge

--- src/4_charged_particle_ret.cpp
+++ src/4_charged_particle_ret.cpp
@@ -47,7 +47,8 @@ std::vector<float> potentials_to_visualized_field(
                       (2 * dt) / c);
             //visualized_field[z * iwidth + r] = E_r;
             //visualized_field[z * iwidth + r] = E_z;
-            visualized_field[z * iwidth + r] = E_r * E_r + E_z * E_z;
+            //visualized_field[z * iwidth + r] = E_r * E_r + E_z * E_z;
+            visualized_field[z * iwidth + r] = phi_prev[z * iwidth + r];
         }
     }
 
@@ -63,7 +64,10 @@ ev::SurfaceData visualized_field_to_surface_data(
     for (size_t i = 0; i < visualized_field.size(); ++i)
     {
         //const float v = 40.0f * visualized_field[i] + 0.5f;
-        const float v = 20000.0f * visualized_field[i];
+        //const float v = 20000.0f * visualized_field[i];
+
+        const float v = 0.75f * visualized_field[i];
+
         glm::vec4 color(v, v, v, 1.0f);
 
         size_t u = i % n_r;
@@ -107,75 +111,141 @@ std::vector<float> iterate_potential(
     const float source_phase = std::numbers::pi / 2.0f;
     const float source_v = 0.5f;
     const float source_amp =
-        (t < 0.1f * l_t ? t / (0.1f * l_t) : 1.0f) * 3.0f *
-        (electric_and_not_magnetic_potential
-             ? 1.0f
-             : source_v * std::cos(source_omega * t + source_phase) / c);
+        3.0f * (electric_and_not_magnetic_potential
+                    ? 1.0f
+                    : source_v * std::cos(source_omega * t + source_phase) / c);
     const float source_movement_amp = source_v / source_omega;
-    const float source_r = 0.0f;
     const float source_z = l_r / 2.0f;
-    const float source_dz =
-        source_movement_amp * std::sin(source_omega * t + source_phase);
     const float source_size = 7.5f;
-    std::vector<float> source = std::vector<float>(n_r * n_r);
-    for (int r = 0; r != iwidth; ++r)
-    {
-        for (int z = 0; z != iwidth; ++z)
-        {
-            const float r1 = std::sqrt(
-                std::pow(source_r - r * dr, 2) +
-                std::pow((source_z + source_dz) - z * dr, 2)
-            );
-            const float r2 = std::sqrt(
-                std::pow(source_r - r * dr, 2) +
-                std::pow((source_z - source_dz) - z * dr, 2)
-            );
-            const float sigma = 0.3;
-            const float sq2 = std::sqrt(2.0f);
-            const float sq2pi = std::sqrt(2.0f * std::numbers::pi);
-            if (r1 <= source_size)
-            {
-                source[z * iwidth + r] +=
-                    source_amp *
-                    std::exp(-std::pow(r1 / source_size / (sq2 * sigma), 2)) /
-                    (sigma * sq2pi);
-            }
-            if (r2 <= source_size)
-            {
-                source[z * iwidth + r] -=
-                    (electric_and_not_magnetic_potential ? 1.0f : -1.0f) *
-                    source_amp *
-                    std::exp(-std::pow(r2 / source_size / (sq2 * sigma), 2)) /
-                    (sigma * sq2pi);
-            }
-        }
-    }
+    //std::vector<float> source = std::vector<float>(n_r * n_r);
+    //for (int r = 0; r != iwidth; ++r)
+    //{
+    //    for (int z = 0; z != iwidth; ++z)
+    //    {
+    //        const float r1 = std::sqrt(
+    //            std::pow(source_r - r * dr, 2) +
+    //            std::pow((source_z + source_dz) - z * dr, 2)
+    //        );
+    //        const float r2 = std::sqrt(
+    //            std::pow(source_r - r * dr, 2) +
+    //            std::pow((source_z - source_dz) - z * dr, 2)
+    //        );
+    //        const float sigma = 0.3;
+    //        const float sq2 = std::sqrt(2.0f);
+    //        const float sq2pi = std::sqrt(2.0f * std::numbers::pi);
+    //        if (r1 <= source_size)
+    //        {
+    //            source[z * iwidth + r] +=
+    //                source_amp *
+    //                std::exp(-std::pow(r1 / source_size / (sq2 * sigma), 2)) /
+    //                (sigma * sq2pi);
+    //        }
+    //        if (r2 <= source_size)
+    //        {
+    //            source[z * iwidth + r] -=
+    //                (electric_and_not_magnetic_potential ? 1.0f : -1.0f) *
+    //                source_amp *
+    //                std::exp(-std::pow(r2 / source_size / (sq2 * sigma), 2)) /
+    //                (sigma * sq2pi);
+    //        }
+    //    }
+    //}
 
     std::vector<float> new_field = std::vector<float>(n_r * n_r);
-    for (int r = 0; r != iwidth; ++r)
+    for (int r_i = 0; r_i != iwidth; ++r_i)
     {
-        for (int z = 0; z != iwidth; ++z)
+        for (int z_i = 0; z_i != iwidth; ++z_i)
         {
-            const int r_minus_dr = (r == 0) ? 0 : (r - 1);
-            const int r_plus_dr = (r == (iwidth - 1)) ? (iwidth - 1) : (r + 1);
-            const int z_minus_dz = (z == 0) ? (iwidth - 1) : (z - 1);
-            const int z_plus_dz = (z == (iwidth - 1)) ? 0 : (z + 1);
+            for (int r_source_i = 0; r_source_i * dr <= source_size;
+                 ++r_source_i)
+            {
+                for (int z_source_i = std::floor(
+                         (source_z - source_movement_amp - source_size) / dr
+                     );
+                     z_source_i <=
+                     std::ceil(
+                         (source_z + source_movement_amp + source_size) / dr
+                     );
+                     ++z_source_i)
+                {
+                    const int num_theta = 4;
+                    for (int theta_source_i = 0; theta_source_i != num_theta;
+                         ++theta_source_i)
+                    {
+                        const float theta_source = theta_source_i * 2.0f *
+                                                   std::numbers::pi / num_theta;
+                        const float z_source = z_source_i * dr;
+                        const float z = z_i * dr;
+                        // The +0.5f is because in the field simulation (3_charged_particle.cpp)
+                        // we shift the calculation at r=0 slightly for numerical stability, and
+                        // we want to be consistent with that calculation.
+                        const float r_source = (r_source_i + 0.5f) * dr;
+                        const float r = (r_i + 0.5f) * dr;
 
-            // In the calculations, 1/r appear, and at r=0 it should blow up.
-            // We could use the L'hopital rule and f(r)=f(-r) assumptions to get
-            // a different update function at r=0, but that solution is very unstable,
-            // at r=0, and creates some numerical instability waves there.
-            const float r_shifted = r + 0.5f;
-            new_field[z * iwidth + r] =
-                (nu_sq * ((1.0f + 1.0f / (2.0f * r_shifted)) *
-                              field[z * iwidth + r_plus_dr] +
-                          (1.0f - 1.0f / (2.0f * r_shifted)) *
-                              field[z * iwidth + r_minus_dr] +
-                          field[z_minus_dz * iwidth + r] +
-                          field[z_plus_dz * iwidth + r]) +
-                 2.0f * (1.0f - 2.0f * nu_sq) * field[z * iwidth + r] -
-                 previous_field[z * iwidth + r]) +
-                source[z * iwidth + r] * dt * dt * c * c;
+                        const float x_source =
+                            r_source * std::sin(theta_source);
+                        const float y_source =
+                            r_source * std::cos(theta_source);
+
+                        const float x = r;
+                        const float y = 0.0f;
+
+                        const float distance = std::sqrt(
+                            std::pow(x_source - x, 2) +
+                            std::pow(y_source - y, 2) +
+                            std::pow(z_source - z, 2)
+                        );
+
+                        // Preventing division by zero,
+                        // because in the discretized version we
+                        // no longer deal with charge densities and real space,
+                        // but point charges and discretized space points.
+                        if (distance >= 0.000001f)
+                        {
+                            const float delay = distance / c;
+                            const float t_delayed = t - delay;
+
+                            const float source_center_z_delayed =
+                                source_z +
+                                source_movement_amp *
+                                    std::sin(
+                                        source_omega * t_delayed + source_phase
+                                    );
+                            // Here we need to take into account the r which is
+                            // unshifted by 0.5f, because that's how we setup the
+                            // values in 3_charged_particle.cpp, and
+                            // we want to be consistent with it.
+                            const float distance_from_source_center = std::sqrt(
+                                std::pow(r_source_i * dr, 2) +
+                                std::pow(source_center_z_delayed - z_source, 2)
+                            );
+                            const float sigma = 0.3;
+                            const float sq2 = std::sqrt(2.0f);
+                            const float sq2pi =
+                                std::sqrt(2.0f * std::numbers::pi);
+                            const float source_density =
+                                source_amp *
+                                std::exp(-std::pow(
+                                    distance_from_source_center / source_size /
+                                        (sq2 * sigma),
+                                    2
+                                )) /
+                                (sigma * sq2pi);
+
+                            const float source_volume =
+                                (2.0f * std::numbers::pi * r_source * dr /
+                                 num_theta) *
+                                dr;
+                            const float source_in_volume =
+                                source_volume * source_density;
+
+                            new_field[z_i * iwidth + r_i] +=
+                                source_in_volume / distance /
+                                (4 * std::numbers::pi);
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -229,14 +299,21 @@ int main(int, char **)
 {
     // Setup parameters of the simulation.
 
-    const int n_t = 1500;
-    const float l_t = 600.0f;
+    //const int n_t = 1500;
+    //const float l_t = 600.0f;
+    const int n_t = 30;
+    const float l_t = 12.0f;
 
     //const size_t n_r = 700;
     //const float l_r = 1000.0f;
     //const int sponge_width = 120;
-    const size_t n_r = 700 - 2 * 120;
-    const float l_r = 1000.0f * n_r / 700;
+
+    //const size_t n_r = 700 - 2 * 120;
+    //const float l_r = 1000.0f * n_r / 700;
+    //const size_t n_r = 460;
+    const size_t n_r = 300;
+    const float l_r = 657.0f;
+
     const float c = 2.0f;
 
     // Running the simulation.
@@ -268,7 +345,7 @@ int visualize_field(
     glm::mat4 projection = glm::ortho(-1.0f, +1.0f, -1.0f, +1.0f);
     surface.value()->set_projection(projection);
 
-    std::string file_name("3_charged_particle.mp4");
+    std::string file_name("4_charged_particle_ret.mp4");
     unsigned int bit_rate = 10000000;
     unsigned int frame_rate = 30;
     glm::uvec2 video_size(1080, 1080);
